82ec56b6870fc83a9ba7ac9f179cd789f5e19ab1
Sourav-thind <souravthind07it@gmail.com>
2025-02-01 20:22:25 -0400

    Adding Gallary Media

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,6 @@ function App() {
       <Hero/>
       <About/>
       <Gallary/>
-      {/* <Highlight/>  */}
 
     </main>
   )
--- src/Utils/index.js
+++ src/Utils/index.js
@@ -6,10 +6,22 @@ import hmv from "/assets/videos/JRG-Hero.mp4";
 import smallmv from "/assets/videos/JRG-Hero.mp4";
 import JRG from "/assets/images/JRG-Logo.png";
 import estimate from "/assets/images/estimate.jpg"
+import cupboard1 from "/assets/images/Cupboard 1.jpg"
+import cupboard2 from "/assets/images/Cupboard 2.jpg"
+import cupboard3 from "/assets/images/Cupboard 3.jpg"
+import cupboard4 from "/assets/images/Cupboard 4.jpg"
+import cupboard5 from "/assets/images/Cupboard 5.jpg"
+import cupboard6 from "/assets/images/Cupboard 6.jpg"
 
 export const heroVideo = hmv;
 export const smallHeroVideo = smallmv;
 export const JRGLOGO = JRG;
 export const Estimate = estimate;
+export const Cupboard1 = cupboard1;
+export const Cupboard2 = cupboard2;
+export const Cupboard3 = cupboard3;
+export const Cupboard4 = cupboard4;
+export const Cupboard5 = cupboard5;
+export const Cupboard6 = cupboard6;
 
 
