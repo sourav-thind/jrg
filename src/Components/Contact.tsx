c36df08846521fb0e8d6499990abc593dcedbb88
Sourav-thind <souravthind07it@gmail.com>
2025-02-09 12:33:47 -0400

    Adding new photos for creations/Projects

--- src/Components/Contact.tsx
+++ src/Components/Contact.tsx
@@ -26,7 +26,7 @@ const Contact = (props: Props) => {
   const onSubmit: SubmitHandler<FormValues> = formData => {
     setShowToast(true);
 
-    
+
     setTimeout(() => {
       setShowToast(false);
     }, 5000);
@@ -51,9 +51,9 @@ const Contact = (props: Props) => {
         subject: '',
         message: ''
       });
-      
-  
- 
+
+
+
     }, (error) => {
       console.log(error);
       alert('Something went wrong')
@@ -64,76 +64,79 @@ const Contact = (props: Props) => {
 
   return (
     <div >
-       <div className="fixed top-[6rem] right-4 z-10 h-[10vh] text-Color1 px-4 py-2 rounded">
-      {showToast && <Toast  />}
-    </div>
+      <div className="fixed top-[6rem] right-4 z-10 h-[10vh] text-Color1 px-4 py-2 rounded">
+        {showToast && <Toast />}
+      </div>
       <div className='bg-Color2  flex mx-auto justify-center  h-[90vh] md:h-[90vh]  '>
-     
-      <div className='flex flex-col items-center  space-y-8 md:space-y-2 '>
-        <h1 className='tracking-[20px] text-2xl md:text-3xl text-drwht uppercase font-semibold mt-6'>Contact</h1>
-        <h2 className=' text-Color1 justify-center items-center  font-semibold text-xl md:font-bold uppercase'> <span> Looking for the perfect solution?</span> <span className='decoration-Color1 underline underline-offset-4 text-drwht'> We&apos;ve got you covered.</span> </h2>
-        <div className='space-y-1 mb-4'>
-          <div className='flex text-drwht text-lg space-x-5 items-center justify-center'>
-            <PhoneIcon className='h-6 w-6 text-Color1' />
-            <p>+1 647-866-3474</p>
-          </div>
-          <div className='flex text-drwht text-lg  space-x-5 items-center justify-center '>
-            <EnvelopeIcon className='h-6 w-6 text-Color1' />
-            <p>souravthind07it@gmail.com</p>
+
+        <div className='flex flex-col items-center  space-y-8 md:space-y-2 '>
+          <h1 className='tracking-[20px] text-2xl md:text-3xl text-drwht uppercase font-semibold mt-6'>Contact</h1>
+          <h2 className=' text-Color1 justify-center items-center  font-semibold text-xl md:font-bold uppercase'> <span> Looking for the perfect solution?</span> <span className='decoration-Color1 underline underline-offset-4 text-drwht'> We&apos;ve got you covered.</span> </h2>
+          <div className='flex flex-col md:flex-row md:pt-16 md:space-x-16 ' >
+            <div className='space-y-1 mb-4'>
+              <div className='flex text-drwht text-lg space-x-5 items-center justify-center'>
+                <PhoneIcon className='h-6 w-6 text-Color1' />
+                <p>+1 647-866-3474</p>
+              </div>
+              <div className='flex text-drwht text-lg  space-x-5 items-center justify-center '>
+                <EnvelopeIcon className='h-6 w-6 text-Color1' />
+                <p>souravthind07it@gmail.com</p>
+              </div>
+            </div>
+         
+          <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col space-y-4 lg:space-y-4 w-[360px]' action="">
+            <div className='space-x-2 flex '>
+              <input className='inputbox h-12 px-2 w-[175px]'
+                type="text" required
+                placeholder='First Name'
+                {...register("firstName")}
+                value={formData.firstName}
+                onChange={(e) =>
+                  setFormData({ ...formData, firstName: e.target.value })
+                } />
+              <input
+                placeholder='Last Name'
+                className='inputbox h-12 px-2 w-[175px]'
+                type="text"
+                {...register("lastName")}
+                value={formData.lastName}
+                onChange={(e) =>
+                  setFormData({ ...formData, lastName: e.target.value })
+                } />
+            </div>
+            <input required
+              placeholder='Email'
+              className='inputbox px-2 h-12'
+              type="email"
+              {...register("email")}
+              value={formData.email}
+              onChange={(e) =>
+                setFormData({ ...formData, email: e.target.value })
+              } />
+            <input
+              placeholder='Subject'
+              className='inputbox h-12 px-2'
+              type="text"
+              {...register("subject")}
+              value={formData.subject}
+              onChange={(e) =>
+                setFormData({ ...formData, subject: e.target.value })
+              } />
+            <textarea required
+              placeholder='Message'
+              className='inputbox h-16 px-2'
+              id="" {...register("message")}
+              value={formData.message}
+              onChange={(e) =>
+                setFormData({ ...formData, message: e.target.value })
+              } />
+            <button className='bg-Color1 px-4 py-2.5 rounded-md text-Color2 hover:text-xl font-semibold text-lg' >Submit</button>
+
+          </form>
           </div>
         </div>
-        <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col space-y-4 lg:space-y-4 w-[360px]' action="">
-          <div className='space-x-2 flex '>
-            <input className='inputbox h-12 px-2 w-[175px]' 
-            type="text" required
-            placeholder='First Name' 
-            {...register("firstName")} 
-            value={formData.firstName} 
-            onChange={(e) =>
-              setFormData({ ...formData, firstName: e.target.value })
-            } />
-            <input 
-            placeholder='Last Name' 
-            className='inputbox h-12 px-2 w-[175px]' 
-            type="text" 
-            {...register("lastName")} 
-            value={formData.lastName} 
-            onChange={(e) =>
-              setFormData({ ...formData, lastName: e.target.value })
-            } />
-          </div>
-          <input required
-          placeholder='Email' 
-          className='inputbox px-2 h-12' 
-          type="email" 
-          {...register("email")} 
-          value={formData.email} 
-          onChange={(e) =>
-            setFormData({ ...formData, email: e.target.value })
-          } />
-          <input 
-          placeholder='Subject' 
-          className='inputbox h-12 px-2' 
-          type="text" 
-          {...register("subject")} 
-          value={formData.subject} 
-          onChange={(e) =>
-            setFormData({ ...formData, subject: e.target.value })
-          } />
-          <textarea required
-          placeholder='Message' 
-          className='inputbox h-16 px-2' 
-          id="" {...register("message")} 
-          value={formData.message} 
-          onChange={(e) =>
-            setFormData({ ...formData, message: e.target.value })
-          } />
-          <button className='bg-Color1 px-4 py-2.5 rounded-md text-Color2 hover:text-xl font-semibold text-lg' >Submit</button>
-       
-        </form>
+        <div></div>
       </div>
-      <div></div>
-    </div>
     </div>
   )
 }
